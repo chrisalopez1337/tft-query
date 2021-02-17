60409a299cffa545c92cc8232a21beb78320caf2
chrisalopez1337 <chrisalopez@outlook.com>
2021-02-16 22:19:27 -0800

    Added method for main request client: getSummonerByPuuid. All tests passing and redis caching functional.

--- tests/main/index.js
+++ tests/main/index.js
@@ -7,7 +7,8 @@ const assert = require('assert');
 describe('Main Request Client: TftQuery', async () => {
     const payload = {
         summonerName: 'scarra',
-        accountId: 'HZRCRfoL7vf8PmW9DFGeQnZx05AFW6dNgxP1t-N3Xw'
+        accountId: 'HZRCRfoL7vf8PmW9DFGeQnZx05AFW6dNgxP1t-N3Xw',
+        puuid: 'vy9iRyl4SENb_sop5qRwuCmHqcV_YU1OKss9E8sbnLzNjA956jwHHfMvnBCx9DdCsSD6IK-gIrAj9g',
     }
     
     const client = new TftQuery('NA', payload, apiKey, true, false);
@@ -22,6 +23,13 @@ describe('Main Request Client: TftQuery', async () => {
     it('Should return valid data: getSummonerByAccountId', async () => {
         const actual = await client.getSummonerByAccountId();
         const path = `https://na1.api.riotgames.com/tft/summoner/v1/summoners/by-account/HZRCRfoL7vf8PmW9DFGeQnZx05AFW6dNgxP1t-N3Xw?api_key=${apiKey}`;
+        const { data } = await axios.get(path);
+        assert.deepEqual(data, actual);
+    });
+
+    it('Should return valid data: getSummonerByPuuid', async () => {
+        const actual = await client.getSummonerByPuuid();
+        const path = `https://na1.api.riotgames.com/tft/summoner/v1/summoners/by-puuid/vy9iRyl4SENb_sop5qRwuCmHqcV_YU1OKss9E8sbnLzNjA956jwHHfMvnBCx9DdCsSD6IK-gIrAj9g?api_key=${apiKey}`;
         const { data } = await axios.get(path);
         assert.deepEqual(data, actual);
     });
