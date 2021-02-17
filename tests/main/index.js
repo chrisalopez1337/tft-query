b8694477ba69a60529e5043299f633767e4b6ea3
chrisalopez1337 <chrisalopez@outlook.com>
2021-02-16 22:30:51 -0800

    Added method for main request client: getSummonerBySummonerId. All tests passing and redis caching functional.

--- tests/main/index.js
+++ tests/main/index.js
@@ -9,6 +9,7 @@ describe('Main Request Client: TftQuery', async () => {
         summonerName: 'scarra',
         accountId: 'HZRCRfoL7vf8PmW9DFGeQnZx05AFW6dNgxP1t-N3Xw',
         puuid: 'vy9iRyl4SENb_sop5qRwuCmHqcV_YU1OKss9E8sbnLzNjA956jwHHfMvnBCx9DdCsSD6IK-gIrAj9g',
+        summonerId: 'IFg_bFm3i52CJVxBvKBuqQmmZ_SqEq52nnInd-PAtfw',
     }
     
     const client = new TftQuery('NA', payload, apiKey, true, false);
@@ -33,4 +34,11 @@ describe('Main Request Client: TftQuery', async () => {
         const { data } = await axios.get(path);
         assert.deepEqual(data, actual);
     });
+
+    it('Should return valid data: getSummonerBySummonerId', async () => {
+        const actual = await client.getSummonerBySummonerId();
+        const path = `https://na1.api.riotgames.com/tft/summoner/v1/summoners/IFg_bFm3i52CJVxBvKBuqQmmZ_SqEq52nnInd-PAtfw?api_key=${apiKey}`;
+        const { data } = await axios.get(path);
+        assert.deepEqual(data, actual);
+    })
 })
